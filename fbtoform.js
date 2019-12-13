72279a1e0b8d6b3d172b1b84f38122da9149f23f
Simonjedi <getfit121@hotmail.com>
2019-12-13 10:23:23 +0000

    updates to fb

--- fbtoform.js
+++ fbtoform.js
@@ -2,7 +2,7 @@
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
      newscript.async = true;
-     newscript.src = 'https://www.gstatic.com/firebasejs/3.0.2/firebase.js';
+     newscript.src = 'https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js';
   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
 })();
 
